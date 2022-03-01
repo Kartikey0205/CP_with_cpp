0e9fdbdbabba51a1fe655de40099fcc0211a0038
Kartikey Dubey <dkartikey1@gmail.com>
2022-03-01 12:37:19 +0530

    added insertion concept in doubly linked list

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -34,6 +34,64 @@ void pushElement(Node **head_ref, int data)
     (*head_ref) = new_node;
 }
 
+// Insert element at Last
+void insertLast(Node **head_ref, int data)
+{
+    Node *last = *head_ref;
+    Node *new_node = new Node();
+    new_node->data = data;
+    new_node->next = NULL;
+
+    // If coming node is first node
+    if ((*head_ref) == NULL)
+    {
+        new_node->prev = NULL;
+        (*head_ref) = new_node;
+        return;
+    }
+
+    // If head is not NULL taht means already have a list
+    while (last->next != NULL)
+    {
+        last = last->next;
+    }
+    last->next = new_node;
+    new_node->prev = last; // THIS IS VERY IMPORTANT CONCEPT OF ADDING PREVIOUS VALUE FOR NEW NODE
+    return;
+}
+
+// Insert element at middle somewhere
+void middleInsert(Node **head_ref, int data, int index)
+{
+    Node *prev_node = (*head_ref);
+    // previous node find kr liye
+    for (int i = 0; i < index; i++)
+    {
+        prev_node = prev_node->next;
+    }
+
+    // Making new node
+    Node *new_node = new Node();
+    new_node->data = data;
+    new_node->next = prev_node->next;
+    prev_node->next = new_node;
+    new_node->prev = prev_node;
+
+    // Still new node k next k prev k arrow remaing so  VERY VERY VERY IMPORTANT PART
+    /*
+     2 CASES -
+            - NEW NODE WHICH IS ENTERED ITS LAST MEANS IT'S NEXT HAS NULL AND
+            - IT HAS NOT NULL
+    */
+
+    // if nULL then no woorry because we had return each thing acording to it
+
+    if (new_node->next != NULL)
+    {
+        new_node->next->prev = new_node;
+    }
+}
+
 //  Printing Doubly Linked List
 void printDoublyLinkedList(Node *head_ref)
 {
@@ -79,6 +137,23 @@ int main()
                 cout << " Enter the value you want to insert " << endl;
                 cin >> value;
                 pushElement(&head, value);
+                printDoublyLinkedList(head);
+
+                break;
+            case 2:
+                cout << " Enter the value you want to insert " << endl;
+                cin >> value;
+                cout << " Enter the index value after which you want to insert " << endl;
+                cin >> index;
+
+                middleInsert(&head, value, index);
+                printDoublyLinkedList(head);
+
+                break;
+            case 3:
+                cout << " Enter the value you want to insert " << endl;
+                cin >> value;
+                insertLast(&head, value);
                 printDoublyLinkedList(head);
 
                 break;
