6586c49db3bc07c73b6273d36238defd5f650e4f
Kartikey Dubey <dkartikey1@gmail.com>
2022-07-02 19:18:26 +0530

    added factors and ascii code

---
+++ Placement_Prep/basic/02_ASCII.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout << "Enter the any character" << endl;
+    cin >> ch;
+    cout << "The ASCII Value of " << ch << " is " << (int)ch << endl;
+    return 0;
+}
