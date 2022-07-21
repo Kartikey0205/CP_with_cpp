b161538361b0479b19605ab8a4086a94c5743bfd
Kartikey Dubey <dkartikey1@gmail.com>
2022-07-21 19:23:53 +0530

    pair basic

---
+++ Placement_Prep/vector_Pair/pair.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    pair<int, char> p;
+    p.first = 3;
+    p.second = 'A';
+
+    cout << p.first << " and " << p.second << endl;
+    return 0;
+}
