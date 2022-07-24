c5924f35aeeb552c5679ee96eaf8b3d48cd46be4
Kartikey Dubey <dkartikey1@gmail.com>
2022-07-24 19:29:55 +0530

    infix to postfix

---
+++ Placement_Prep/stack/infix_To_Postfix.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int precedence(char c)
+{
+    if (c == '^')
+        return 3;
+    else if (c == '*' || c == '/')
+        return 2;
+    else if (c == '+' || c == '-')
+        return 1;
+    else
+        return -1;
+}
+string infix_To_Postfix(string s)
+{
+    string res;
+    stack<int> st;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z')
+        {
+
+            res += s[i];
+        }
+        else if (s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+        else if (s[i] == ')')
+        {
+            while (!st.empty() && st.top() != '(')
+            {
+                res += st.top();
+                st.pop();
+            }
+            if (!st.empty())
+            {
+                st.pop();
+            }
+        }
+        else
+        {
+            while (!st.empty() && precedence(st.top()) > precedence(s[i]))
+            {
+                res += st.top();
+                st.pop();
+            }
+            st.push(s[i]);
+        }
+    }
+    while (!st.empty())
+    {
+        res += st.top();
+        st.pop();
+    }
+
+    return res;
+}
+int main()
+{
+    string s = "(a-b/c)*(a/k-l)";
+    cout << "Infix  " << s << " has postfix = " << infix_To_Postfix(s) << endl;
+    return 0;
+}
