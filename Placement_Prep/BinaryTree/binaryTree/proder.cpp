e92df4b8c5bdf39b945c48c1d1e13a31bf0af905
Kartikey Dubey <dkartikey1@gmail.com>
2022-07-26 13:05:51 +0530

    PreOrder Traversal

---
+++ Placement_Prep/BinaryTree/binaryTree/proder.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preOrder(Node *&root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+int main()
+{
+
+    Node *root = new Node(3);
+    root->left = new Node(6);
+    root->right = new Node(10);
+
+    /*
+                3
+            6       10
+
+    */
+    root->left->left = new Node(89);
+    root->left->right = new Node(9);
+
+    /*
+                3
+            6       10
+        89      9
+    */
+    cout << "Pre Order of Tree is ";
+    preOrder(root);
+    return 0;
+}
